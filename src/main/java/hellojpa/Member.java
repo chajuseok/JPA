3a43096503121136cbe7d173b2804db6ac84e1f5
Cha <ckwntjr128@naver.com>
2022-06-27 21:47:47 +0900

    first commit

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+//@Table(name = "USER")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Member() {
+    }
+}
